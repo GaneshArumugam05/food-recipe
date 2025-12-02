400220d1d6b2882e0c5953432ab0be45804ae8b0
GaneshArumugam05 <ganesharumugam2k@gmail.com>
2025-12-02 10:24:40 +0530

    changes made

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  ase: "/food-recipe/",
+  base: "/food-recipe/",
   plugins: [react()],
 })
